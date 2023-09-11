ddd712c54278ae1cde21dfb29fcda480797c4179
Asteises <p.nikitaev@gmail.com>
2023-09-11 21:50:19 +0300

    fix

--- src/main/java/ru/asteises/checkbrackets/service/impl/BracketsServiceImpl.java
+++ src/main/java/ru/asteises/checkbrackets/service/impl/BracketsServiceImpl.java
@@ -7,75 +7,80 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.asteises.checkbrackets.service.BracketsService;
 
+import java.util.Stack;
+
+
 @Slf4j
 @AllArgsConstructor
 @Getter
 @Setter
 @Service
 public class BracketsServiceImpl implements BracketsService {
-
+    // A ( A (  A  ) A) A (.
+    // A ( A ( A. ) ).
     private int bracketCount;
-    private boolean isText;
+    private Stack<Character> charsBetweenBrackets;
+
     @Override
     public Boolean checkBrackets(String text) {
-        char[] chars = text.toCharArray();
-        if (!isLastCharIsPoint(chars)) {
-            log.info("Текст должен заканчиваться точкой");
-            return false;
-        }
-        for (char checkChar: chars) {
-            if (isLetterOrDigit(checkChar)) {
-                log.info("Это действительно буква или цифра: {}", checkChar);
-                setText(true);
-            }
-            if (isItLastPoint(checkChar)) {
-                log.info("Нельзя поставить точку внутри скобок");
-                return false;
-            }
-            if (isItBracket(checkChar)) {
-                if (!isItOpenBracket(checkChar) && getBracketCount() == 0) {
+        char checkChar;
+        for (int i = 0; i < text.length(); i++) {
+            checkChar = text.charAt(i);
+            if (isBracket(checkChar)) {
+                if (isOpenBracket(checkChar) && !charsBetweenBrackets.isEmpty()) {
+                    bracketCount += 1;
+                    log.info("Найдена открывающая скобка, увеличиваем флаг: {}", bracketCount);
+                    charsBetweenBrackets.clear();
+                }
+                if (!isOpenBracket(checkChar) && !charsBetweenBrackets.isEmpty() && bracketCount != 0) {
+                    bracketCount -= 1;
+                    log.info("Найдена закрывающая скобка, пара закрылась: {}", bracketCount);
+                } else if (!isOpenBracket(checkChar) && bracketCount != 0 && charsBetweenBrackets.isEmpty()) {
+                    log.info("Между скобками нет текста");
+                    return false;
+                } else if (!isOpenBracket(checkChar) && !charsBetweenBrackets.isEmpty() && bracketCount == 0) {
                     log.info("Первая скобка в тексте оказалась закрывающей - не годиться");
                     return false;
                 }
-                if (isItOpenBracket(checkChar) && isText()) {
-                    setBracketCount(getBracketCount() + 1);
-                    log.info("Найдена открывающая скобка, увеличиваем флаг: {}", getBracketCount());
-                    setText(false);
-                }
-                // Если это закрывающая скобка и между скобками был текст, то уменьшаем count
-                if (!isItOpenBracket(checkChar) && isText()) {
-                    setBracketCount(getBracketCount() - 1);
-                    log.info("Найдена закрывающая скобка, пара закрылась: {}", getBracketCount());
-                }
             }
+            if (isPointBetweenBracket(checkChar)) {
+                log.info("Нельзя поставить точку внутри скобок");
+                return false;
+            }
+            charsBetweenBrackets.push(checkChar);
         }
-        if (getBracketCount() == 0) {
-            log.info("Все пары скобок закрылись, или это был текст без скобок. В любом случае, поздравляю!");
+        if (bracketCount == 0) {
+            log.info("Все пары скобок закрылись, или это был текст без скобок. В любом случае, все хорошо. Ура!");
             return true;
+        } else if (bracketCount > 0) {
+            log.info("Где-то не закрыли скобку: {}", bracketCount);
+            return false;
         } else {
-            log.info("Где-то не закрыли скобку: {}", getBracketCount());
+            log.info("Закрывающих скобок  больше чем нужно: {}", bracketCount);
             return false;
         }
     }
 
-    // Проверяем, что это открывающая скобка
-    public Boolean isItOpenBracket(char checkChar) {
+    /**
+     * Метод проверяет, что принятый символ это открывающая скобка.
+     *
+     * @param checkChar char
+     * @return boolean
+     */
+    public Boolean isOpenBracket(char checkChar) {
         return checkChar == '(';
     }
-    // Проверяем, что символ, это буква или цифра
-    public Boolean isLetterOrDigit(char c) {
-        return Character.isLetter(c) || Character.isAlphabetic(c) || Character.isDigit(c);
-    }
-    // Проверяем, что текст заканчивается точкой
-    public Boolean isLastCharIsPoint(char[] chars) {
-        return chars[chars.length - 1] == '.';
-    }
-    // Проверяем, что точка появляется до закрытия каждой второй скобки
-    public Boolean isItLastPoint(char checkChar) {
-        return checkChar != '.' || getBracketCount() % 2 == 0;
+
+    /**
+     * Метод проверяет, чтобы внутри пары скобок не было точки.
+     * @param checkChar
+     * @return
+     */
+    public Boolean isPointBetweenBracket(char checkChar) {
+        return checkChar != '.' || bracketCount > 0;
     }
     // Проверяем, что символ это скобка
-    public Boolean isItBracket(char checkChar) {
+    public Boolean isBracket(char checkChar) {
         return checkChar == '(' || checkChar == ')';
     }
 }
